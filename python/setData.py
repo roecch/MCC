9789bae2063ccf5e7f507a4dcce37db50a1bfd28
roecch <90482248+roecch@users.noreply.github.com>
2022-04-04 19:51:10 -0400

    change image

--- python/setData.py
+++ python/setData.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import mysql.vendor
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -16,8 +17,3 @@ mydb.commit()
 mydb.close()
 
 
-def set_ranboo_data():
-    query = "REPLACE INTO RANBOO (MCCNUM, COLOR, ACERACE_TIME, ACERACE_PLACE, BB_KILLS, BB_WINS, HITW, PT_HUNTER, PT_RUNNER, " \
-            "SB_SURVIVAL, SB_KILLS, SOT_SOLO, SG_SURVIVAL, SG_KILLS, TGTTOS_1, TGTTOS_2, TGTTOS_3, TGTTOS_4, TGTTOS_5, TGTTOS_6) " \
-            "VALUES (15, 'pink', '06:21:20', 9, 6, null, null, null, null, null, null, null, null, '21:37:37', 1, 414, null, null, null, null)"
-    cur.execute()
--- python/setTables.py
+++ python/setTables.py
@@ -1,14 +1,6 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Iamc00l!rsb897h4",
-    database="MCC"
-)
-
-cur = mydb.cursor(buffered=True)
-
+global all_players
 all_players = ['5up', 'antfrost', 'asnazum', 'awesamdude', 'badboyhalo', 'bbpaws', 'bitzel', 'captainpuffy', 'captainsparklez', 'connoreatspants',
                'cubfan135', 'cxlvxn', 'dantdm', 'dethridge', 'dream', 'drgluon', 'elainaexe', 'eret', 'f1nn5ter', 'falsesymmetry', 'florianfluke',
                'fruitberries', 'fundy', 'fwhip', 'geenelly', 'geminitay', 'georgenotfound', 'gizzygazza', 'goodtimeswithscar', 'grain', 'graser10',
@@ -22,29 +14,52 @@ all_players = ['5up', 'antfrost', 'asnazum', 'awesamdude', 'badboyhalo', 'bbpaws
                'theorionsound', 'tommyinnit', 'toxxicsupport', 'tubbo', 'vapekit', 'vgumiho', 'vikkstar123', 'vixella', 'voiceoverpete', 'wilbur',
                'wisp', 'wolv21', 'yammyxox', 'yeetdaisie'];
 
-cur.execute("SELECT TABLE_NAME from information_schema.tables where TABLE_SCHEMA = 'mcc'")
-playerExists = cur.fetchall()
 
-for player in all_players:
+def __setNullTables__(cur):
+    cur.execute("SELECT TABLE_NAME from information_schema.tables where TABLE_SCHEMA = 'mcc'")
+    playerExists = cur.fetchall()
 
-    if (player,) not in playerExists:
-        createQuery = ['CREATE TABLE IF NOT EXISTS ', player, '(MCCNUM VARCHAR(20), COLOR VARCHAR(20), ACERACE_TIME TIME, ACERACE_PLACE INT, BB_KILLS INT, '
-                                                              'BB_WINS INT, BINGO VARCHAR(20), BM VARCHAR(20), GR VARCHAR(20), HITW TIME, PT_HUNTER VARCHAR'
-                                                              '(20), PT_RUNNER VARCHAR(20), PW_FIRST VARCHAR(20), PW_COURSE VARCHAR(20), SB_SURVIVAL TIME, '
-                                                              'SB_KILLS INT, SOT_SOLO VARCHAR(20), SKYBLOCKE VARCHAR(20), SG_SURVIVAL INT, SG_KILLS INT, '
-                                                              'TGTTOS_1 INT, TGTTOS_2 INT, TGTTOS_3 INT, TGTTOS_4 INT, TGTTOS_5 INT, TGTTOS_6 INT);']
-        query = "".join(createQuery)
-        cur.execute(query)
+    for player in all_players:
 
-        for num in range(20):
-            setAllNull = ['INSERT INTO ', player,
-                          ' VALUES (', str(num + 1), ', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, '
-                                                     'null, null, null, null, null, null, null, null);']
-            query = "".join(setAllNull)
+        if (player,) not in playerExists:
+            createQuery = ['CREATE TABLE IF NOT EXISTS ', player, '(MCCNUM VARCHAR(20), COLOR VARCHAR(20), ACERACE_TIME TIME, ACERACE_PLACE INT, BB_KILLS '
+                                                                  'INT, BB_WINS INT, BINGO VARCHAR(20), BM VARCHAR(20), GR VARCHAR(20), HITW TIME, '
+                                                                  'PT_HUNTER VARCHAR (20), PT_RUNNER VARCHAR(20), PW_FIRST VARCHAR(20), PW_COURSE '
+                                                                  'VARCHAR(20), SB_SURVIVAL TIME, SB_KILLS INT, SOT_SOLO VARCHAR(20), SKYBLOCKE VARCHAR('
+                                                                  '20), SG_SURVIVAL INT, SG_KILLS INT, TGTTOS_1 INT, TGTTOS_2 INT, TGTTOS_3 INT, TGTTOS_4 '
+                                                                  'INT, TGTTOS_5 INT, TGTTOS_6 INT);']
+            query = "".join(createQuery)
             cur.execute(query)
 
-for x in cur:
-    print(x)
+            for num in range(20):
+                setAllNull = ['INSERT INTO ', player,
+                              ' VALUES (', str(num + 1), ', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, '
+                                                         'null, null, null, null, null, null, null, null, null, null);']
+                query = "".join(setAllNull)
+                cur.execute(query)
 
-mydb.commit()
-mydb.close()
+
+def main():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Iamc00l!rsb897h4",
+        database="MCC"
+    )
+
+    cur = mydb.cursor(buffered=True)
+
+    __setNullTables__(cur)
+
+    try:
+        for x in cur:
+            print(x)
+    except Exception:
+        pass
+
+    mydb.commit()
+    mydb.close()
+
+
+if __name__ == "__main__":
+    main()
